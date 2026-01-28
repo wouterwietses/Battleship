1ad8a439dae08d63abe50b17160678e272127f00
Wouter Wietses <wouter.wietses@ns.nl>
2026-01-28 09:20:09 +0100

    feat: place carrier on board

--- Sources/Battleship/BattleshipBoard.swift
+++ Sources/Battleship/BattleshipBoard.swift
@@ -1,5 +1,14 @@
-enum XAxis: CaseIterable {
-    case one, two, three, four, five, six, seven, eight, nine, ten
+enum XAxis: Int, CaseIterable {
+    case one = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
 }
 
 enum YAxis: CaseIterable {
@@ -8,7 +17,7 @@ enum YAxis: CaseIterable {
     // swiftlint:enable identifier_name
 }
 
-struct Coordinate {
+struct Coordinate: Hashable {
     // swiftlint:disable identifier_name
     let x: XAxis
     let y: YAxis
@@ -16,13 +25,35 @@ struct Coordinate {
 }
 
 enum CellValue {
-    case water
+    case water, ship
 }
 
-struct BattleshipBoard {
+enum ShipType {
+    case carrier
+}
+
+enum Orientation {
+    case horizontal
+}
+
+final class BattleshipBoard {
     let playerName: String
 
-    func value(at _: Coordinate) -> CellValue {
-        .water
+    private var grid: [Coordinate: CellValue] = [:]
+
+    init(playerName: String) {
+        self.playerName = playerName
+    }
+
+    func value(at coordinate: Coordinate) -> CellValue {
+        grid[coordinate] ?? .water
+    }
+
+    func place(ship _: ShipType, at _: Coordinate, orientation _: Orientation) {
+        grid[Coordinate(x: .one, y: .A)] = .ship
+        grid[Coordinate(x: .two, y: .A)] = .ship
+        grid[Coordinate(x: .three, y: .A)] = .ship
+        grid[Coordinate(x: .four, y: .A)] = .ship
+        grid[Coordinate(x: .five, y: .A)] = .ship
     }
 }
--- Tests/BattleshipTests/BattleshipTests.swift
+++ Tests/BattleshipTests/BattleshipTests.swift
@@ -1,5 +1,6 @@
-@testable import Battleship
 import Testing
+
+@testable import Battleship
 
 @Suite("As a player I want to see an empty game board So that I can visualize the battlefield")
 struct EmptyBoardTests {
@@ -16,5 +17,27 @@ struct EmptyBoardTests {
         let board = BattleshipBoard(playerName: "Player 1")
 
         #expect(board.playerName == "Player 1")
+    }
+}
+
+@Suite("As a player I want to place my fleet on the board So that I can prepare for battle")
+struct PlaceShipsTests {
+    @Test
+    func placeCarrier() async throws {
+        let board = BattleshipBoard(playerName: "Player 1")
+
+        board.place(ship: .carrier, at: Coordinate(x: .one, y: .A), orientation: .horizontal)
+
+        let coordinates = [
+            Coordinate(x: .one, y: .A),
+            Coordinate(x: .two, y: .A),
+            Coordinate(x: .three, y: .A),
+            Coordinate(x: .four, y: .A),
+            Coordinate(x: .five, y: .A)
+        ]
+        for coordinate in coordinates {
+            let value = board.value(at: coordinate)
+            #expect(value == .ship)
+        }
     }
 }
